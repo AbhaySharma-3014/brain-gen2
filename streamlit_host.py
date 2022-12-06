493c170fdecc759560032e664514ac6b4d0840fb
Abhay Sharma <110305361+AbhaySharma-3014@users.noreply.github.com>
2022-12-07 03:06:24 +0530

    Update streamlit_host.py

--- streamlit_host.py
+++ streamlit_host.py
@@ -40,36 +40,6 @@ if uploaded_file is not None:
 
 
 # ************************************************** PreProcessing **************************************************#
-#     resized = cv2.resize(opencv_image,(224,224))
-#     image=resized
-#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#     gray = cv2.GaussianBlur(gray, (5, 5), 0)
-
-#     # Threshold the image, then perform a series of erosions + dilations to remove any small regions of noise
-#     thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
-#     thresh = cv2.erode(thresh, None, iterations=2)
-#     thresh = cv2.dilate(thresh, None, iterations=2)
-
-#     # Find contours in thresholded image, then grab the largest one
-#     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#     cnts = imutils.grab_contours(cnts)
-#     c = max(cnts, key=cv2.contourArea)
-
-#     # Find the extreme points
-#     extLeft = tuple(c[c[:, :, 0].argmin()][0])
-#     extRight = tuple(c[c[:, :, 0].argmax()][0])
-#     extTop = tuple(c[c[:, :, 1].argmin()][0])
-#     extBot = tuple(c[c[:, :, 1].argmax()][0])
-
-#     # crop new image out of the original image using the four extreme points (left, right, top, bottom)
-#     new_image = image[extTop[1]:extBot[1], extLeft[0]:extRight[0]]
-
-#     image = cv2.resize(new_image, dsize=(240, 240), interpolation=cv2.INTER_CUBIC)
-#     image = image / 255.
-
-#     image = image.reshape((1, 64, 64, 3))      
-    
-    #*********
     resized = cv2.resize(opencv_image,(224,224))
 
     image=resized
@@ -99,8 +69,7 @@ if uploaded_file is not None:
     image = cv2.resize(new_image, dsize=(240, 240), interpolation=cv2.INTER_CUBIC)
     image = image / 255.
 
-    image = image.reshape((0, 64, 64, 3))
-    #*********
+    image = image.reshape((1, 240, 240, 3))
 # *************************************************************************************************************************#
     
 
