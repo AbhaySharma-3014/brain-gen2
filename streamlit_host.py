f2520a919f6e543e378139d3663474c4ea4f04af
Abhay Sharma <110305361+AbhaySharma-3014@users.noreply.github.com>
2022-12-07 02:58:42 +0530

    Update streamlit_host.py

--- streamlit_host.py
+++ streamlit_host.py
@@ -149,86 +149,44 @@ if uploaded_file is not None:
 
             
 #********************************************Things for showing region of tumor********************************************#
-#     Img = uploaded_file
-#     curImg=uploaded_file
+            img=uploaded_file
+            Img = opencv_image
+            # curImg = np.array(img)
+            curImg=opencv_image
 
-#     gray = cv2.cvtColor(np.array(Img), cv2.COLOR_BGR2GRAY)
-#     [ret, thresh] = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+            gray = cv2.cvtColor(np.array(Img), cv2.COLOR_BGR2GRAY)
+            [ret, thresh] = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-#     # noise removal
-#     kernel = np.ones((3, 3), np.uint8)
-#     opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
-#     curImg = opening
+            # noise removal
+            kernel = np.ones((3, 3), np.uint8)
+            opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+            curImg = opening
 
-#     # Finding sure background area
-#     sure_bg = cv2.dilate(curImg, kernel, iterations=3)
+            # sure background area
+            sure_bg = cv2.dilate(curImg, kernel, iterations=3)
 
-#     # Finding sure foreground area
-#     dist_transform = cv2.distanceTransform(curImg, cv2.DIST_L2, 5)
-#     [ret, sure_fg] = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
+            # Finding sure foreground area
+            # curImg = image.img_to_array(curImg, dtype='uint8')
+            dist_transform = cv2.distanceTransform(curImg, cv2.DIST_L2, 5)
+            [ret, sure_fg] = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
 
-#     # Finding unknown region (Subtracting)
-#     sure_fg = np.uint8(sure_fg)
-#     unknown = cv2.subtract(sure_bg, sure_fg,dtype=cv2.CV_32F)
+            # Find unknown region
+            sure_fg = np.uint8(sure_fg)
+            unknown = cv2.subtract(sure_bg, sure_fg,dtype=cv2.CV_32F)
 
+            # Marker labelling
+            ret, markers = cv2.connectedComponents(sure_fg)
 
-#     # Marker labelling
-#     ret, markers = cv2.connectedComponents(sure_fg)
+            # Add one to all labels so that sure background is not 0, but 1
+            markers = markers + 1
 
-#     # Add one to all labels so that sure background is not 0, but 1
-#     markers = markers + 1
+            # Now mark the region of unknown with zero
+            markers[unknown == 255] = 0   
+            markers = cv2.watershed(opencv_image, markers)
 
-#     # Now mark the region of unknown with zero
-#     markers[unknown == 255] = 0   
-#     markers = cv2.watershed(opencv_image, markers)
+            opencv_image[markers == -1] = [255, 0, 0]
 
-#     opencv_image[markers == -1] = [255, 0, 0]
-
-#     tumorImage = cv2.cvtColor(opencv_image, cv2.COLOR_HSV2BGR)
-#     curImg = tumorImage            
-#     st.image(curImg)
-    
-    
-    #*******
-    img=uploaded_file
-    Img = opencv_image
-    # curImg = np.array(img)
-    curImg=opencv_image
-
-    gray = cv2.cvtColor(np.array(Img), cv2.COLOR_BGR2GRAY)
-    [ret, thresh] = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-
-    # noise removal
-    kernel = np.ones((3, 3), np.uint8)
-    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
-    curImg = opening
-
-    # sure background area
-    sure_bg = cv2.dilate(curImg, kernel, iterations=3)
-
-    # Finding sure foreground area
-    # curImg = image.img_to_array(curImg, dtype='uint8')
-    dist_transform = cv2.distanceTransform(curImg, cv2.DIST_L2, 5)
-    [ret, sure_fg] = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
-
-    # Find unknown region
-    sure_fg = np.uint8(sure_fg)
-    unknown = cv2.subtract(sure_bg, sure_fg,dtype=cv2.CV_32F)
-
-    # Marker labelling
-    ret, markers = cv2.connectedComponents(sure_fg)
-
-    # Add one to all labels so that sure background is not 0, but 1
-    markers = markers + 1
-
-    # Now mark the region of unknown with zero
-    markers[unknown == 255] = 0   
-    markers = cv2.watershed(opencv_image, markers)
-
-    opencv_image[markers == -1] = [255, 0, 0]
-
-    tumorImage = cv2.cvtColor(opencv_image, cv2.COLOR_HSV2BGR)
-    curImg = tumorImage            
-    st.image(curImg)
-    #*******
+            tumorImage = cv2.cvtColor(opencv_image, cv2.COLOR_HSV2BGR)
+            curImg = tumorImage            
+            st.image(curImg)
 # *************************************************************************************************************************#
